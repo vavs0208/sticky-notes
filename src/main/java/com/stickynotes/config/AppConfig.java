2009164bf904575426aa0b2773a3d83403521576
ASINGHS <47058252+ASINGHS@users.noreply.github.com>
2020-05-14 15:28:50 +0200

    changes for Config

--- src/main/java/com/stickynotes/config/AppConfig.java
+++ src/main/java/com/stickynotes/config/AppConfig.java
@@ -1,13 +1,21 @@
 package com.stickynotes.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 /* @Import annotation is used to import defined configuration files */
 /* If there are multiple configuration files then use like this @Import({ Module1Config.class, Module2Config.class }) */
 /* To enable auto creation of service beans */
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @Import(DatabaseConfig.class) 
 public class AppConfig {
+	
+	@Bean @LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+}
 
 }
